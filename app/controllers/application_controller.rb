c2cf24c44507b1467f795cc1174e45f0480e71fc
Phisith.L (Beckham) <59072804+phisith@users.noreply.github.com>
2023-05-10 18:30:01 +0700

    add new route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# app controller
 class ApplicationController < ActionController::Base
+  def hello
+    render html: '¡hola, mundo!'
+  end
+
+  def good_bye
+    render html: 'GoodBye, World!'
+  end
 end
